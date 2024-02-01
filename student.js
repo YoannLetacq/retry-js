f77ea40dca5a6b971928734917e2e2aa3e7f4752
yyoannle <yoann.letacq1@gmail.com>
2024-02-01 13:35:04 +0100

    add student exercices

---
+++ student.js
@@ -0,0 +1,39 @@
+/* -------------------------------------------------------------------------- */
+/*                                 Exercise 1                                */
+/* -------------------------------------------------------------------------- */
+
+// In the Contact section, when clicking on the "Introduce yourself" button, 
+// get the text typed in the input and display it in the middle of the following sentence: 
+// "Nice to meet you [put here the input data] 👋! Thanks for introducing yourself." 
+// Also, the <p>, <input> & <button> elements have to disappear after the button has been clicked.
+console.log("hello student")
+
+/* -------------------------------------------------------------------------- */
+/*                                 Exercice 2                                 */
+/* -------------------------------------------------------------------------- */
+
+// When clicking on a card , open a modal (web page element that displays in front of and deactivates all other page content)
+// window that will show the whole article ; 
+// the modal will be closed either when clicking on a "Close" button, or when the "Escape" key is pressed.
+
+/* -------------------------------------------------------------------------- */
+/*                                 Exercice 3                                 */
+/* -------------------------------------------------------------------------- */
+
+// set the header text content with a random quote every time the page is loaded, and then every 10 seconds. 
+// Use this Chuck Norris API: https://api.chucknorris.io/# to fetch his most inspiring sayings and display them in your own page!
+
+/* -------------------------------------------------------------------------- */
+/*                                 Exercice 4                                 */
+/* -------------------------------------------------------------------------- */
+
+// Create a function that generates a random array based on the number you input and fill with the size of the array. 
+// When you click on the 'Array Exercise' button, it should display an alert with the generated array and the sum of its elements.
+
+/* -------------------------------------------------------------------------- */
+/*                                 Exercice 5                                 */
+/* -------------------------------------------------------------------------- */
+
+// Write a function that takes a number as input and show the power of this number. When you click on the 'Function Exercise' button, enter a value in the input field, 
+// and understand how the function uses this value to produce a result.
+ 
